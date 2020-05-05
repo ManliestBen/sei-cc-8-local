fd9ca68677625898b67dfe999b021b520db6dc19
ManliestBen <benjamintmanley@gmail.com>
2020-05-04 22:28:06 -0500

    update solution code

--- unit-1/memory-game-plus/solution-code/js/main.js
+++ unit-1/memory-game-plus/solution-code/js/main.js
@@ -1,8 +1,4 @@
-const resetDiv = document.getElementById('resetDiv');
-const buttonDiv = document.getElementById('difficultyButtons');
-const messageEl = document.getElementById('message');
-const playArea = document.getElementById('playArea');
-const resetBtn = document.getElementById('resetButton');
+/*------Constants------*/
 
 const difficulty = {
     "Easy": 5,
@@ -10,10 +6,22 @@ const difficulty = {
     "Hard": 15
 }
 
+/*------Variables------*/
+
 let winTime, seconds, matchCount, firstCard, secondCard, card1Val, card1Idx, card2Val, card2Idx, waitingForTimeout;
 let turn = 1;
 let deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"];
 let cardsInPlay = [];
+
+/*------Cached Element References------*/
+
+const resetDiv = document.getElementById('resetDiv');
+const buttonDiv = document.getElementById('difficultyButtons');
+const messageEl = document.getElementById('message');
+const playArea = document.getElementById('playArea');
+const resetBtn = document.getElementById('resetButton');
+
+/*------Event Listeners------*/
 
 buttonDiv.addEventListener('click', function(evt) {
     if (evt.target.innerText) {
@@ -32,7 +40,10 @@ resetBtn.addEventListener('click', function() {
 playArea.addEventListener('click', function(evt) {
         handlePick(evt);
 })
-    
+
+
+/*------Functions------*/
+
 init();
 
 setInterval(addSecond, 1000);
