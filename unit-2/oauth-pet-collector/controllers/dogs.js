4a98781ad0e37ec10590953e7e758bdafb1874a3
ManliestBen <benjamintmanley@gmail.com>
2020-05-24 00:19:43 -0500

    add materialize cards to index dogs page

--- unit-2/oauth-pet-collector/controllers/dogs.js
+++ unit-2/oauth-pet-collector/controllers/dogs.js
@@ -4,11 +4,14 @@ const Dog = require('../models/dog');
 module.exports = {
     index,
     myDogs,
-    new: newDog
+    new: newDog,
+    create
 };
 
 function index(req, res) {
-    res.render('dogs/index', {user: req.user})
+    Dog.find({}, function(err, dogs) {
+        res.render('dogs/index', {user: req.user, dogs: dogs})
+    })
 }
 
 function myDogs(req, res) {
@@ -17,4 +20,16 @@ function myDogs(req, res) {
 
 function newDog(req, res) {
     res.render('dogs/new', {user: req.user})
+}
+
+function create(req, res) {
+    req.body.owner = req.user._id;
+    req.body.ownerName = req.user.name;
+    const newDog = new Dog(req.body);
+    newDog.save(function(err) {
+        if (err) return res.redirect('/dogs/new');
+        console.log(newDog);
+        res.redirect('/dogs/myDogs');
+    })
+    
 }
--- unit-2/oauth-pet-collector/routes/dogs.js
+++ unit-2/oauth-pet-collector/routes/dogs.js
@@ -4,5 +4,6 @@ const dogsCtrl = require('../controllers/dogs');
 router.get('/', dogsCtrl.index);
 router.get('/myDogs', dogsCtrl.myDogs);
 router.get('/new', dogsCtrl.new);
+router.post('/add', dogsCtrl.create);
 
 module.exports = router;
