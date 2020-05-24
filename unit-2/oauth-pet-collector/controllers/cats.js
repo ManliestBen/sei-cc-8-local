f6302f892d288faa32d83bd56aa1a9d485996596
ManliestBen <benjamintmanley@gmail.com>
2020-05-24 02:29:47 -0500

    add cat add/index functionality

--- unit-2/oauth-pet-collector/controllers/cats.js
+++ unit-2/oauth-pet-collector/controllers/cats.js
@@ -3,7 +3,8 @@ const User = require('../models/user');
 module.exports = {
     index,
     myCats,
-    new: newCat
+    new: newCat,
+    create
 };
 
 function index(req, res) {
@@ -11,9 +12,21 @@ function index(req, res) {
 }
 
 function myCats(req, res) {
-    res.render('cats/myCats', {user: req.user})
+    User.findById(req.user._id, function(err, usersCats) {
+        res.render('cats/myCats', {user: req.user, cats: usersCats.cats})
+    })
 }
 
 function newCat(req, res) {
     res.render('cats/new', {user: req.user})
+}
+
+function create(req, res) {
+    User.findById(req.user._id, function(err, userAdding) {
+        req.body.owner = req.user.name;
+        userAdding.cats.push(req.body);
+        userAdding.save(function(err) {
+            res.redirect('/cats/myCats')
+        })
+    });
 }
--- unit-2/oauth-pet-collector/routes/cats.js
+++ unit-2/oauth-pet-collector/routes/cats.js
@@ -4,5 +4,6 @@ const catsCtrl = require('../controllers/cats');
 router.get('/', catsCtrl.index);
 router.get('/myCats', catsCtrl.myCats);
 router.get('/new', catsCtrl.new);
+router.post('/add', catsCtrl.create);
 
 module.exports = router;
